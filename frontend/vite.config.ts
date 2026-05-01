b0feb79a34cccfe227237902c2d7fe0421b24b1a
ivanruan <b.ivan.ruiz@gmail.com>
2026-04-30 23:28:20 -0600

    chore: setup docker infrastructure, nginx, and initialize vue 3

---
+++ frontend/vite.config.ts
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    host: '0.0.0.0', // Permite que Docker exponga el puerto hacia tu máquina
+    port: 5173,
+    watch: {
+      usePolling: true // Asegura que Vite detecte los cambios de archivos en vivo (Hot Reload)
+    }
+  }
+})
